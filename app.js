c62cdda1dd1be90334f506330d5b32087210e974
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-10-20 02:48:33 +0530

    commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+  res.send("Hello");
+});
+
+app.post("/", function(req,res){
+
+
+    
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
+ 
