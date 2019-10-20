4f8be33da57cdc9d36b001825f8906dc5031bba0
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-10-20 10:44:58 +0530

    commit

--- app.js
+++ app.js
@@ -3,18 +3,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 
+
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  res.render('index', {});
 });
 
-app.post("/", function(req,res){
+app.get("/login", function(req, res){
+  res.render('login', {});
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.get("/sellerlogin", function(req, res){
+  res.render('sellerlogin', {});
+});
+
+
+
+
+app.listen(4000, function(){
+  console.log("Server started on port 4000.");
 });
  
