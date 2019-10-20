83bf3747d67ca9d5e878d236f2c6ee77c3e8ce97
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-10-20 10:56:28 +0530

    commit

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const ejs = require("ejs");
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
 
 
 
