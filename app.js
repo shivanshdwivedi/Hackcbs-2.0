d532b77ddfa7e6d8f5d8d3d1ad4f679b64bb830e
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-10-20 06:18:39 +0530

    commit

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req,res){
