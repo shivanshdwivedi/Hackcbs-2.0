8f67387ef54b21acf27ae3a9a37db66c883884ea
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-10-20 07:23:31 +0530

    commit

--- app.js
+++ app.js
@@ -8,20 +8,10 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/login.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req,res){
-
-if(u===p)
-{
-  rate = (rate) - (0.5)(rate);
-}
-else
-{
-  rate = rate -(0.7)(rate);
-}
-    
 });
 
 app.listen(3000, function(){
