024e2566263467ea89d8316dba920f4dd3204a1b
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-10-20 11:17:10 +0530

    commit

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //jshint esversion:6
+
 const express = require("express");
+const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const app = express();
-const ejs = require("ejs");
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 
 
 
